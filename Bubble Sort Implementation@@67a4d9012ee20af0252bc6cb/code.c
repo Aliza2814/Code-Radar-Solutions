d004ac40a517607368678e793367a47103265ca8
Aliza2814 <alizaji2006@gmail.com>
2025-02-25 11:24:40 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
-int calsum(int x,int y,int z);
-int main()
-{
-    int a,b,c,sum;
-    printf("enter no's");
-    scanf("%d%d%d",&a,&b,&c);
-    sum= calsum(a,b,c);
-    printf("%d",sum);
+
+int calsum(int x, int y, int z);  // Function prototype
+
+int main() {
+    int a, b, c, sum;
+    
+    // Better prompt
+    printf("Enter three numbers: ");
+    scanf("%d%d%d", &a, &b, &c);
+    
+    sum = calsum(a, b, c);
+    
+    // Better output message
+    printf("Sum = %d\n", sum);
 
     return 0;
 }
 
-int calsum(int x,int y,int z)
-{
-    int d;
-    d=x+y+z;
-    return (d);
+// Function to calculate sum
+int calsum(int x, int y, int z) {
+    return x + y + z;  // Direct return
 }
+
